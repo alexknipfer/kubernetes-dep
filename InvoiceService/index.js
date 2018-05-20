a2e84bf76fec205fbe7b5e53f222abeb09ee08b5
alexknipfer <alexanderknipfer@gmail.com>
2018-05-19 19:27:38 -0500

    Update service

--- InvoiceService/index.js
+++ InvoiceService/index.js
@@ -11,16 +11,6 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use('/api/getWeatherByZip/', getWeatherByZip)
 
-app.get('/api/invoices/:id', (req, res) => {
-  const id = parseInt(req.params.id)
-  res.json({
-    id: id,
-    ref: `INV-${id}`,
-    amount: id * 100,
-    balance: id * 100 - 10
-  })
-})
-
 const port = process.env.PORT || 8080
 
 app.listen(port, () => {
