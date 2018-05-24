aedd4186697cf925987c2e6075c3fff310428ad7
alexknipfer <alexanderknipfer@gmail.com>
2018-05-23 22:44:11 -0500

    Cleanup and update config

--- weather-service/src/inversify.config.ts
+++ weather-service/src/inversify.config.ts
@@ -1,34 +1,13 @@
 import 'reflect-metadata'
 import { Container, injectable, decorate } from 'inversify'
-import { ExpressServer } from './server/server'
-import { OpenWeatherClientImpl } from './clients/OpenWeatherClientImpl'
-import { RegistrableController } from './interfaces/RegistrableController'
-import { InversifyTypes } from './models/InversifyTypes'
-import { WeatherByZipRouteController } from './routeControllers/WeatherByZipRouteController'
-import { DefaultRouteController } from './routeControllers/DefaultRouteController'
+export class InversifyConfiguration {
+  public static container = new Container()
 
-const container = new Container()
-
-// Services
-bindClass(InversifyTypes.Server, ExpressServer)
-bindClass(InversifyTypes.OpenWeatherClient, OpenWeatherClientImpl)
-
-// Registrable controllers
-bindClass<RegistrableController>(
-  InversifyTypes.Controller,
-  WeatherByZipRouteController
-)
-bindClass<RegistrableController>(
-  InversifyTypes.Controller,
-  DefaultRouteController
-)
-
-function bindClass<InterfaceType>(type: symbol, classToBind: any) {
-  decorate(injectable(), classToBind)
-  container
-    .bind<InterfaceType>(type)
-    .to(classToBind)
-    .inSingletonScope()
+  public static decorateAndBind<InterfaceType>(type: symbol, classToBind: any) {
+    decorate(injectable(), classToBind)
+    this.container
+      .bind<InterfaceType>(type)
+      .to(classToBind)
+      .inSingletonScope()
+  }
 }
-
-export default container
